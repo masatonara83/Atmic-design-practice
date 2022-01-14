2c898db515af0e2326abff88f2ce403bcc78ed48
masatonara <93502709+masatonara83@users.noreply.github.com>
2022-01-14 11:58:31 +0900

    完成

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
 
 const users = [...Array(10).keys()].map((val) => {
   return {
@@ -15,22 +16,16 @@ const users = [...Array(10).keys()].map((val) => {
   };
 });
 
-const user = {
-  name: "わんこ",
-  image: "https://source.unsplash.com/G8cB8hY3yvU",
-  email: "123456@com",
-  phone: "000-000-0987",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export const Users = () => {
   return (
     <SContainaer>
       <h2>ユーザー一覧</h2>
       <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
     </SContainaer>
   );
 };
@@ -40,4 +35,12 @@ const SContainaer = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `;
